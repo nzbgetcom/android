91dc6dad924851d4a9f8c97dafa1f3aef98bfed4
phnzb <pavel@nzbget.com>
2024-07-23 15:30:39 +0300

    Add openjdk-8 install

--- scripts/prepare-sdk.sh
+++ scripts/prepare-sdk.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
-sudo apt update
-sudo apt install -y sdkmanager apksigner
+sudo apt-get update
+sudo apt-get install -y sdkmanager apksigner openjdk-8-jdk
 sudo sdkmanager --install "build-tools;28.0.3"
 sudo sdkmanager --install "platforms;android-28"
