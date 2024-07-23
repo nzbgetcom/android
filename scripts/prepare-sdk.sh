0c4cd07f0e688f4dd3be2080988083550b717d6d
phnzb <pavel@nzbget.com>
2024-07-23 15:49:59 +0300

    Add update-alternatives

--- scripts/prepare-sdk.sh
+++ scripts/prepare-sdk.sh
@@ -2,5 +2,6 @@
 set -e
 sudo apt-get update
 sudo apt-get install -y sdkmanager apksigner openjdk-8-jdk
+sudo update-alternatives --set java /usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java
 sudo sdkmanager --install "build-tools;28.0.3"
 sudo sdkmanager --install "platforms;android-28"
